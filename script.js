8068f99efae20c2d4994412faf2af49819b09c0a
Istamovashakhzoda <istamovashahzoda@gmail.com>
2024-06-02 19:24:50 +0500

    git finished succesfully this project cool

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// function my_function() {
+//   let array = [];
+
+//   let a = prompt("Enter the first name");
+//   let b = prompt("Enter the second name");
+
+//   array[0] = a;
+//   array[1] = b;
+
+//   let longestName = array.reduce((longest, name) => {
+//     if (name.length > longest.length) {
+//       return name;
+//     } else {
+//       return longest;
+//     }
+//   });
+
+//   if (longestName.length > 0) {
+//     console.log("The longest name is: ", longestName);
+//   } else {
+//     console.log("No names found");
+//   }
+// }
+
+// my_function();
+
 function my_function() {
   let array = [];
 
@@ -7,19 +33,5 @@ function my_function() {
   array[0] = a;
   array[1] = b;
 
-  let longestName = array.reduce((longest, name) => {
-    if (name.length > longest.length) {
-      return name;
-    } else {
-      return longest;
-    }
-  });
-
-  if (longestName.length > 0) {
-    console.log("The longest name is: ", longestName);
-  } else {
-    console.log("No names found");
-  }
+  let longestName = array.reduce;
 }
-
-my_function();
