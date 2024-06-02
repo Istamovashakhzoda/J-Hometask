6c03cbc1f5b126038a6d97d0804b635e01413adc
Istamovashakhzoda <istamovashahzoda@gmail.com>
2024-06-02 16:00:34 +0500

    git finished one home task in javascript

---
+++ script.js
@@ -0,0 +1,25 @@
+function my_function() {
+  let array = [];
+
+  let a = prompt("Enter the first name");
+  let b = prompt("Enter the second name");
+
+  array[0] = a;
+  array[1] = b;
+
+  let longestName = array.reduce((longest, name) => {
+    if (name.length > longest.length) {
+      return name;
+    } else {
+      return longest;
+    }
+  });
+
+  if (longestName.length > 0) {
+    console.log("The longest name is: ", longestName);
+  } else {
+    console.log("No names found");
+  }
+}
+
+my_function();
