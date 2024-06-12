49acf47e773794edb28d8b1ce08f3409a12adc14
Istamovashakhzoda <istamovashahzoda@gmail.com>
2024-06-12 22:19:46 +0500

    git finished the this small project

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//   const h1Element = document.querySelector("h1");
-//   h1Element.textContent = "Salom Dunyo";
-// });
+document.addEventListener("DOMContentLoaded", function () {
+  const counterElement = document.getElementById("counter");
+  const incrementBtn = document.getElementById("incrementBtn");
+  const decrementBtn = document.getElementById("decrementBtn");
+  const resetBtn = document.getElementById("resetBtn");
+
+  let counterValue = 0;
+
+  incrementBtn.addEventListener("click", function () {
+    counterValue++;
+    counterElement.textContent = counterValue;
+  });
+
+  decrementBtn.addEventListener("click", function () {
+    counterValue--;
+    counterElement.textContent = counterValue;
+  });
+
+  resetBtn.addEventListener("click", function () {
+    counterValue = 0;
+    counterElement.textContent = counterValue;
+  });
+});
